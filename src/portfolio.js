8f2fda5dd0ae5c668d165ba6f3aa8aed90b06009
Amrit Kalsi <64483302+Brainmantis07@users.noreply.github.com>
2022-11-03 00:28:25 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -36,10 +36,7 @@ const socialMediaLinks = {
   github: "https://github.com/Brainmantis07",
   linkedin: "https://www.linkedin.com/in/saadpasta/",
   gmail: "amritkalsi7@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  hackerank: "https://www.hackerrank.com/ak4839",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
